f11717a23c5d1c76d615edcad46fef09cf293688
Physlex <al.willet02@gmail.com>
2023-12-04 20:43:52 -0700

    nearly finished lab 3

--- pa3q2p2C.m
+++ pa3q2p2C.m
@@ -1,59 +1,61 @@
 % Global State
 l = [0.8; 0.7];
 mode = 1;
-iters = 1000;
+iters = 50;
 
 % Intial Conditions
-theta = invKin3D(l, rand(3,1), rand(3,1), iters, mode);
+theta0 = [0.2259; 0.1707; 0.2277];
+theta = invKin3D(l, theta0, [1; sqrt(2)/2; 0], iters, mode);
 [pos, ~] = evalRobot3D(l, theta);
 pos = pos';
 
-% Again, because it's random we can't really say anything specific. But,
-% the updating function will swap itself around quickly. The way to solve
-% this solution is to plan a path by swivel, linearly interpolating a
-% longer path around the arm rather than having it attempt to cross
-% directly to that point. To accomplish this, we can have intermediary
-% points it moves to before it executes a diametrically opposed movement,
-% or we could move the largest angles at a time first so that it swivels
-% the whole body, then the largest arm segment, then the next largest and
-% so on. These all prevent swiveling directly across the ceneter of the
-% axis.
+disp(pos);
+
+% Plot for iter
+plotRobot3D(l, theta);
+pause;
+clf;
 
 %% Error State Version
 
-% Plot for iter
-plotRobot3D(l, theta);
-pause;
-clf;
-
 % Get a new set of angles
-pos = -pos;
-theta = invKin3D(l, rand(3, 1), pos, iters, mode);
+end_pos = -pos;
+x_end_pos = linspace(pos(1), end_pos(1), 100);
+y_end_pos = linspace(pos(2), end_pos(2), 100);
+z_end_pos = linspace(pos(3), end_pos(3), 100);
+end_pos_path = [x_end_pos; y_end_pos; z_end_pos];
 
-% Solve for condition number
-J_inv = inv(J);
-cn = norm(J) * norm(J_inv);
+max_cn = 0;
+for k = 1:100
+    theta = invKin3D(l, theta, end_pos_path(1:3, k), iters, mode);
 
-% Display relavent results
-disp(pos);
-disp(cn);
+    plotRobot3D(l, theta);
+    clf;
 
-%% Path Planned Version
+    % Solve for condition number
+    [~, J] = evalRobot3D(l, theta);
+    J_inv = inv(J);
+    cn = norm(J) * norm(J_inv);
+
+    max_cn = max(cn, max_cn);
+end
+
+% Find worst conditioning
+disp(max_cn);
 
 
-% Plot for iter
-plotRobot3D(l, theta);
-pause;
-clf;
+%% ANSWER
 
-% Get a new set of angles
-pos = -pos;
-theta = invKin3D(l, rand(3, 1), pos, iters, mode);
-
-% Solve for condition number
-J_inv = inv(J);
-cn = norm(J) * norm(J_inv);
-
-% Display relavent results
-disp(pos);
-disp(cn);
+% Conditioning becomes an issue as the robot eases towards the center
+% platform. (upwards of 300 in some cases).
+%
+% There are a few solutions to this:
+% - We can plan a path around the robot that ends in the correct position.
+% - We can force big rotations to occur first, then smaller ones second and
+%   even smaller third. Ex. Move the base, then the elbow, then the wrist.
+% - We can take the nearest radial path, meaning we can define a minimum
+%   domain that the robot is allowed to operate near itself, and do a
+%   collision check to determine if it will hit itself. If it does, we plan
+%   a path around itself using A* or some similarly appropriate path
+%   planning algorithm. If we want manual control, as stated in the lab, we
+%   could always manually implement bezier's, though that would be a pain.
